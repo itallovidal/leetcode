f34e27acfd810afc060cb2930a388fcd5b84ca3e
italloVidal <itallovidalll@gmail.com>
2024-02-27 13:21:49 -0300

    09 palindrome easy

---
+++ easy/09 Palindrome/index.ts
@@ -0,0 +1,13 @@
+function isPalindrome(x: number): boolean {
+    const y = x.toString()
+    console.log(y)
+    let reversedNumber = ''
+
+    for(let i = y.length - 1; i >= 0; i--){
+        reversedNumber += y[i]
+    }
+    return !!y.match(reversedNumber)
+}
+
+
+isPalindrome(101)
